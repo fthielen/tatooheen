7edc4d2cb5c000371a521d68b6100be62995a4b6
Frederick Thielen <frederick@Fredericks-MacBook-Air.local>
2021-09-16 21:11:02 +0200

    last test

--- R/fill_create_tx_schedule.R
+++ R/fill_create_tx_schedule.R
@@ -3,10 +3,9 @@
 #' @param regimen_phase The regimen phase for which to fill `tatooheen::create_tx_schedule()`
 #' @param df A data.frame in the form of `tx_regimens.xlsx`
 #' @param cyc_max The maximum number of cycles when no cycle duration is given (i.e. until progression in some instances)
-#' @keywords CADTH
-#' @export fill_create_tx_schedule
+#' @export fill_create_tx_schedule1
 
-fill_create_tx_schedule <- function(regimen_name,
+fill_create_tx_schedule1 <- function(regimen_name,
                                     regimen_phase,
                                     df = df_tx_regimen,
                                     cyc_max = 100){
@@ -48,23 +47,6 @@ fill_create_tx_schedule <- function(regimen_name,
         tx5_days <-  df %>%
                 select(days_5) %>%
                 pull %>% as.character()
-
-        # Fill
-        # create_tx_schedule(cyc_dur_d = df$cyc_dur,
-        #                    cyc_n = ifelse(is.na(df$max_cyc), cyc_max, df$max_cyc),
-        #                    tx1_d = if(
-        #                            !is.na(tx1_days)) as.numeric(unlist(strsplit(tx1_days, ","))),
-        #                    tx2_d = if(
-        #                            !is.na(tx2_days)) as.numeric(unlist(strsplit(tx2_days, ","))),
-        #                    tx3_d = if(
-        #                            !is.na(tx3_days)) as.numeric(unlist(strsplit(tx3_days, ","))),
-        #                    tx_names = tx_names,
-        #                    tx4_d = if(
-        #                            !is.na(tx4_days)) as.numeric(unlist(strsplit(tx4_days, ","))),
-        #                    tx5_d = if(
-        #                            !is.na(tx5_days)) as.numeric(unlist(strsplit(tx5_days, ","))),
-        #
-        # )
 
         create_tx_schedule(cyc_dur_d = df$cyc_dur,
                            cyc_n = ifelse(is.na(df$max_cyc), cyc_max, df$max_cyc),
