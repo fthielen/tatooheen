14d1d30d6d9eadf47b9126db8acac7f040fa4898
Frederick Thielen <frederick@Fredericks-MacBook-Air.local>
2021-09-17 13:45:45 +0200

    join_tx_schedule extended to up to 5 phase

--- R/join_tx_schedule.R
+++ R/join_tx_schedule.R
@@ -2,16 +2,22 @@
 #' @param tx_schedule_ph1 A treatment schedule for phase I
 #' @param tx_schedule_ph2 A treatment schedule for phase II
 #' @param tx_schedule_ph3 A treatment schedule for phase III
+#' @param tx_schedule_ph4 A treatment schedule for phase IV
+#' @param tx_schedule_ph5 A treatment schedule for phase V
 #' @keywords CADTH
 #' @export join_tx_schedule
 
 join_tx_schedule <- function(tx_schedule_ph1,
                              tx_schedule_ph2,
-                             tx_schedule_ph3 = NULL){
+                             tx_schedule_ph3 = NULL,
+                             tx_schedule_ph4 = NULL,
+                             tx_schedule_ph5 = NULL){
         bind_rows(tx_schedule_ph1,
                   tx_schedule_ph2) %>%
 
                 {if(!is.null(tx_schedule_ph3)) bind_rows(., tx_schedule_ph3) else . } %>%
+                {if(!is.null(tx_schedule_ph4)) bind_rows(., tx_schedule_ph4) else . } %>%
+                {if(!is.null(tx_schedule_ph5)) bind_rows(., tx_schedule_ph5) else . } %>%
                 replace(is.na(.), 0) %>%
                 group_by(phase, cycle) %>%
                 mutate(cycle = cur_group_id()) %>%
