44401d7898eca8b8697c5b37702ef546b7bcfed0
Frederick Thielen <thielen@eshpm.eur.nl>
2023-07-20 15:05:46 +0200

    reference script. When uncommented will run automatically.

--- R/deploy_update.R
+++ R/deploy_update.R
@@ -4,17 +4,17 @@
 #devtools::document()
 
 # Build
-devtools::build()
-
-# Commit and push
-
-# Add all changes to the staging area
-system("git add .")
-
-# Commit the changes
-system(paste('git commit -m "',
-             paste('Auto commit message on', Sys.Date()),
-             '"', sep = ''))
-
-# Push the changes
-system("git push origin master")
+# devtools::build()
+#
+# # Commit and push
+#
+# # Add all changes to the staging area
+# system("git add .")
+#
+# # Commit the changes
+# system(paste('git commit -m "',
+#              paste('Auto commit message on', Sys.Date()),
+#              '"', sep = ''))
+#
+# # Push the changes
+# system("git push origin master")
